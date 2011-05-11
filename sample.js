3f4a829fbf687360939aa79e102a36837a3d6f70
Takuya Iida <dbiida@gmail.com>
2011-05-11 23:03:25 +0900

    The sample of JavaScript

---
+++ sample.js
@@ -0,0 +1,49 @@
+importClass(java.lang.System)
+importClass(com.mongodb.Mongo, com.mongodb.BasicDBObject, com.mongodb.DBCursor)
+importClass(com.mongodb.rhino.BSON, com.mongodb.rhino.JSON)
+
+// Prepare collection of MongoDB.
+var connection = new Mongo()
+var db = connection.getDB('mydb');
+var collection = db.getCollection('testCollection');
+
+function createDoc(idx) {
+    var doc = new BasicDBObject();
+    doc.put("name", "Name-" + idx);
+    doc.put("age",  7);
+    doc.put("sayName", "function(){return 'My name is Name-' + " + idx + ";}");
+    return doc;
+}
+
+// Insert JSON objects.
+for(idx = 0; idx < 10; idx++) {
+    collection.insert(createDoc(idx));    
+}
+
+// var document = BSON.from(collection.find());
+var documents = collection.find();
+
+while(documents.hasNext()) {
+  var object = BSON.from(documents.next());
+  for(elm in object) {
+    System.out.print(" ");
+    if(eval("object." + elm).toString().indexOf("function(") == 0) {
+      System.out.print("says '" + eval(object.sayName)() + " !!'");
+    } else {
+      System.out.print(elm + " = " + eval("object." + elm));
+    }
+  }
+  System.out.println("");
+}
+
+
+
+
+// Print them all.
+//while(documents.hasNext()) {
+//  var object = BSON.from(documents.next());
+//  System.out.print("name = " + object.name);
+//  System.out.print(", age = " + object.age);
+//  System.out.println(", '" + eval(object.sayName)() + "'");
+//}
+// -- End of script.
